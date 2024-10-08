8a5a6ddc4861910495ae09a32378697a012ae048
yoonju88 <yoonju.t@gmail.com>
2024-10-08 15:39:16 +0200

    Added responsive design on tour page

--- app/tours/page.tsx
+++ app/tours/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Image from 'next/image';
 const url = 'https://www.course-api.com/react-tours-project';
 
 type Tour = {
@@ -22,13 +23,29 @@ async function ToursPage() {
   return (
     <section>
       <h1 className='text-3xl mb-4'>Tours</h1>
-      {data.map((tour) => {
-        return (
-          <Link key={tour.id} href={`/tours/${tour.id}`} className='hover:text-blue-500'>
-            <h2>{tour.name}</h2>
-          </Link>
-        )
-      })}
+      <div className='grid md:grid-cols-2 gap-8'>     
+        {data.map((tour) => {
+          return (
+            <Link 
+              key={tour.id} 
+              href={`/tours/${tour.id}`} 
+              className='hover:text-blue-500'
+            >
+              <div className='relative h-48 mb-2'>
+                  <Image 
+                    src={tour.image} 
+                    alt={tour.name} 
+                    fill 
+                    priority 
+                    sizes='(max-width:768px) 100vw, (max-width:1200px) 50vw,33vw'
+                    className='object-cover rounded' 
+                  />
+              </div>
+              <h2>{tour.name}</h2>
+            </Link>
+          )
+        })}
+      </div>
     </section>
   )
 }
