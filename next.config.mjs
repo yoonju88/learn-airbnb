7d4d761a90e8d0987e227a332daf2ca4cb800175
yoonju88 <yoonju.t@gmail.com>
2024-10-08 15:25:04 +0200

    add remote images on next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
+
+
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+       remotePatterns: [
+        {
+            protocol: 'https',
+            hostname: 'www.course-api.com',
+            port: '',
+            pathname:'/**',
+        },
+       ], 
+    },
+};
 
 export default nextConfig;
