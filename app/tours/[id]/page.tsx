69425cffaea3b2b1f32bdf42949561523753ad94
yoonju88 <yoonju.t@gmail.com>
2024-10-08 15:24:19 +0200

    gestion local image and remote image

--- app/tours/[id]/page.tsx
+++ app/tours/[id]/page.tsx
@@ -1,9 +1,34 @@
+import mapsImage from '@/images/pexels-andrew-2859169.jpg'
+import Image from 'next/image'
+const url = 'https://www.course-api.com/images/tours/tour-1.jpeg';
+
 function page({params}:{params:{id:string}}) {
   return (
     <div>
-      <h1 className="text-4xl">ID :{params.id}</h1>
+        <h1 className="text-4xl">ID :{params.id}</h1>
+        <section className='flex gap-x-4 mt-4'>
+            <div>
+                <Image 
+                    src={mapsImage} 
+                    alt='maps' width={192} 
+                    height={192} 
+                    className='w-48 h-48 object-cover rounded'
+                    priority
+                />
+                <h1>local image</h1>
+            </div>
+            <div>
+                <Image 
+                    src={url}  
+                    alt='tour' 
+                    width={192} 
+                    height={192 }
+                    className='w-48 h-48 object-cover rounded'
+                />
+            </div>
+            <h1>remote image</h1>
+        </section>
     </div>
   )
 }
-
 export default page
