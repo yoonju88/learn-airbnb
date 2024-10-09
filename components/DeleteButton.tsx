9b5edaf2fa01c58e0a45a6a08e29324bd01191ca
yoonju88 <yoonju.t@gmail.com>
2024-10-09 16:39:32 +0200

    Use a hidden input to get ID for deleting from theusers list

--- components/DeleteButton.tsx
+++ components/DeleteButton.tsx
@@ -1,7 +1,13 @@
+import { deleteUser, removeUser } from "@/utils/actions"
+
 function DeleteButton({id}: {id:string}) {
   return (
-    <form>
-        <button type="submit" className="bg-red-500 text-white text-xs rounded p-2">
+    <form action={deleteUser}>
+        <input type='hidden' name='id' value={id} />
+        <button 
+            type="submit" 
+            className="bg-red-500 text-white text-xs rounded p-2"
+        >
             Delete
         </button>
     </form>
